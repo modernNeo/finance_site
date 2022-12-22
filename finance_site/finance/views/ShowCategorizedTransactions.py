2bf845cf1be3a7214fb1118b716977fde335ca2a
Jace Manshadi <jmanshad@protonmail.com>
2022-12-21 17:16:31 -0800

    fixing how to display the categorized totals to avoid duplicate entries

--- finance_site/finance/views/ShowCategorizedTransactions.py
+++ finance_site/finance/views/ShowCategorizedTransactions.py
@@ -3,7 +3,7 @@ import datetime
 from django.shortcuts import render
 from django.views.generic.base import View
 
-from finance.models.TransactionModels import FinalizedTransaction
+from finance.models.TransactionModels import FinalizedTransaction, TransactionCategory
 
 
 class ShowCategorizedTransactions(View):
@@ -14,29 +14,12 @@ class ShowCategorizedTransactions(View):
         months = list(reversed(months))
         categorized_transactions = {}
         category_totals = {}
-        categories_i_care_about = [
-            "Numeris",
-            "Government Deposit",
-            "Misc Income",
-            "Rent",
-            "Beauty/Vanity",
-            "Chomp",
-            "Household Necessities",
-            "Homeless People",
-            "Spanish Tutoring",
-            "Pho Friday",
-            "Yearly Subscription",
-            "Recurring Monthly Bills",
-            "Charity Donations",
-            "Music/Movies",
-            "Leisure",
-            "Online Purchases",
-            "Theatres/New DVD releases",
-            "Home-Office",
-            "Medical",
-            "Automated Student Loans Payment",
-            "Student Loans"
-        ]
+        category_index = {}
+        categories_i_care_about = list(
+            TransactionCategory.objects.all().filter(
+                starred=True
+            ).order_by('order_number').values_list("category", flat=True)
+        )
 
         for transaction in transactions:
             if transaction.category is not None:
@@ -44,14 +27,14 @@ class ShowCategorizedTransactions(View):
                     categorized_transactions[transaction.get_month] = []
                 categorized_transactions[transaction.get_month].append(transaction)
                 if transaction.category.category in categories_i_care_about:
-                    increase_price(category_totals, transaction.get_month, transaction.category.category, transaction.price)
+                    increase_price(category_index, category_totals, transaction.get_month, transaction.category.category, transaction.price)
                 if transaction.category.category == "Partial" or transaction.category.category == "Categorized Elsewhere":
                     for item in transaction.finalizeditem_set.all():
                         if transaction.get_month not in categorized_transactions:
                             categorized_transactions[transaction.get_month] = []
                         categorized_transactions[transaction.get_month].append(item)
                         if item.category.category in categories_i_care_about:
-                            increase_price(category_totals, item.get_month, item.category.category, item.price)
+                            increase_price(category_index, category_totals, item.get_month, item.category.category, item.price)
                         for reimbursement_receipt in item.reimbursements_mapping_set.all():
                             if transaction.get_month not in categorized_transactions:
                                 categorized_transactions[transaction.get_month] = []
@@ -60,6 +43,7 @@ class ShowCategorizedTransactions(View):
                             reimbursement_transaction = reimbursement_receipt.reimbursement_transaction
                             if reimbursement_transaction.category.category in categories_i_care_about:
                                 increase_price(
+                                    category_index,
                                     category_totals,
                                     reimbursement_transaction.get_month,
                                     reimbursement_transaction.category.category,
@@ -72,7 +56,7 @@ class ShowCategorizedTransactions(View):
                     categorized_transactions[transaction.get_month] = []
                 categorized_transactions[transaction.get_month].append(refund_receipt)
                 if refund_receipt.category.category in categories_i_care_about:
-                    increase_price(category_totals, refund_receipt.get_month, refund_receipt.category.category, refund_receipt.price)
+                    increase_price(category_index, category_totals, refund_receipt.get_month, refund_receipt.category.category, refund_receipt.price)
             original_receipts = transaction.get_transactions_this_transaction_is_refunding()
             for original_receipt in original_receipts:
                 original_receipt.pre_pend = "--REFUNDS--"
@@ -80,7 +64,7 @@ class ShowCategorizedTransactions(View):
                     categorized_transactions[transaction.get_month] = []
                 categorized_transactions[transaction.get_month].append(original_receipt)
                 if original_receipt.category.category in categories_i_care_about:
-                    increase_price(category_totals, original_receipt.get_month, original_receipt.category.category, original_receipt.price)
+                    increase_price(category_index, category_totals, original_receipt.get_month, original_receipt.category.category, original_receipt.price)
 
             corresponding_internal_transfers = transaction.corresponding_internal_transfer_mapping_set.all()
             for corresponding_internal_transfer in corresponding_internal_transfers:
@@ -92,7 +76,7 @@ class ShowCategorizedTransactions(View):
                 internal_transfer = corresponding_internal_transfer.internal_transfer
                 if internal_transfer.category.category in categories_i_care_about:
                     increase_price(
-                        category_totals, internal_transfer.get_month, internal_transfer.category.category,
+                        category_index, category_totals, internal_transfer.get_month, internal_transfer.category.category,
                         internal_transfer.price
                     )
 
@@ -105,7 +89,7 @@ class ShowCategorizedTransactions(View):
                 e_transfer = corresponding_etransfer.e_transfer
                 if e_transfer.category.category in categories_i_care_about:
                     increase_price(
-                        category_totals, e_transfer.get_month, e_transfer.category.category, e_transfer.price
+                        category_index, category_totals, e_transfer.get_month, e_transfer.category.category, e_transfer.price
                     )
 
         for month, value in category_totals.items():
@@ -130,20 +114,24 @@ def add_month(category_dict, month_str):
         category_dict[month_str] = [{}]
 
 
-def add_category(category_dict, month_str, category_str):
+def add_category(category_index, category_dict, month_str, category_str):
     add_month(category_dict, month_str)
-
-    current_row = len(category_dict[month_str]) - 1
-    categories_on_current_page = len(category_dict[month_str][current_row])
-
-    if categories_on_current_page == 4:
-        category_dict[month_str].append({})
+    if not (category_str in category_index and month_str in category_index[category_str]):
         current_row = len(category_dict[month_str]) - 1
-    if category_str not in category_dict[month_str][current_row]:
-        category_dict[month_str][current_row][category_str] = 0
+        categories_on_current_page = len(category_dict[month_str][current_row])
+
+        if categories_on_current_page == 4:
+            category_dict[month_str].append({})
+            current_row = len(category_dict[month_str]) - 1
+        if category_str not in category_dict[month_str][current_row]:
+            category_dict[month_str][current_row][category_str] = 0
+        if category_str not in category_index:
+            category_index[category_str] = {month_str : current_row}
+        if month_str not in category_index[category_str]:
+            category_index[category_str][month_str] = current_row
 
 
-def increase_price(category_dict, month_str, category_str, price):
-    add_category(category_dict, month_str, category_str)
-    current_index = len(category_dict[month_str]) - 1
+def increase_price(category_index, category_dict, month_str, category_str, price):
+    add_category(category_index, category_dict, month_str, category_str)
+    current_index = category_index[category_str][month_str]
     category_dict[month_str][current_index][category_str] += price
